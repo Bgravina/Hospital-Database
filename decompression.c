eb37a6627669cb43fcc6ebb075de54222764e2ae
congkhanh236 <50691908+congkhanh236@users.noreply.github.com>
2019-05-23 15:17:58 +1000

    Update decompression.c

--- decompression.c
+++ decompression.c
@@ -11,14 +11,16 @@ int main(void){
 	int length; 
 	int countdup;
 
-	
+/* enter the string needed for decompress */	
 	printf("Enter a string >\n");
 	scanf("%s", password);
-	
+/* get the length of the string */	
 	length = strlen(password);
-	strcpy(storepassword, password);
+
 	
 	
+/* as the string is a combination of characters and '\0' at the end */
+/* the program will compare the first digit with the one next to it, including '\0' */
 	
 	
 	
@@ -28,13 +30,15 @@ int main(void){
 
 		for(j = i + 1; j < length + 1; j++ )
 		{
+			/* if the digit needed to compare is a number after a word */ 
 			if(  (password[j] >= '0') && (password[j] <= '9' ))
 			{
-				
+				/* change the number in the string into a single number */
 				int x = password[j] - '0';
-				
+				/* when that number is greater than 1 */
 				if ( x > 1)
 				{
+					/* printing the digit corresponding to the number after it */
 					for( k = 0 ; k < x  ; k ++ )
 					{
 						printf("%c", password[i]);
@@ -43,11 +47,12 @@ int main(void){
 				}
 				
 			}
+			/* when their is a word after a digit, the program will look at that and the end to determine to print the word within */ 
 			else if (  (password[i] <= '0' || password[i] >= '9') || ( (password[j] <= '0' || password[j] >= '9') && ( password [j] != 0) &&( password [i] == 0) ) )
 			{
 					printf("%c", password[i]);
 			}
-			
+			/* increment the comparasion digit as it should be comparing between every single one */
 			i = i + 1;
 				
 			
