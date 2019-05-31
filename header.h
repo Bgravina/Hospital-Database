c2aa69de32a4c4b54ff039406e9daf7f45810c25
congkhanh236 <50691908+congkhanh236@users.noreply.github.com>
2019-05-31 23:30:12 +1000

    Add files via upload

---
+++ header.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+	Macros
+*******************************************************************************/
+#define MAX_NUM_PATIENTS 100
+#define MAX_DEPARTMENT_LEN 20
+#define MAX_LEVEL_LEN 2
+#define MAX_ROOM_LEN 3
+#define MAX_ROOM 200
+#define	MAX_MED_LEN 20
+#define MAX_NAME_LEN 20
+#define MAX_ID_LEN 6
+#define MAX_PASS_LEN 16
+#define LEVEL_MIN 0         /* Min level of hospital, can be changed according
+                               to hospital */
+#define LEVEL_MAX 5         /* Max level of hospital, can be changed according
+                               to hospital */
+#define KEY 'K'				/* Key for encrypting and decrypting data */
